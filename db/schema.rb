8256fe6ec7117cc049066437f5659a3e6c119843
saasbook <saasbook@saasbook.(none)>
2013-09-30 02:28:21 +0530

    shifted dev to sqlite and prod to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(version: 20130924013132) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
   create_table "weekdays", force: true do |t|
     t.string   "name"
