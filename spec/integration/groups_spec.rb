5a2eda9d0ded0284af409ae3c50e772baf4e2427
Ankur <ankothari@gmail.com>
2013-10-15 16:38:01 +0530

    can create gorup using ajax post and create method itsel calls get_group_code

--- spec/integration/groups_spec.rb
+++ spec/integration/groups_spec.rb
@@ -1,36 +1,23 @@
 require "spec_helper"
-
+ include SessionsHelper
 describe "Groups" do
 
   it "signed in user should be able to create a group" do
     user = FactoryGirl.create(:user)
-	  visit '/'
-	  click_link "Log In"
-
-	  fill_in "session_email",with: user.email
-	  fill_in "session_password",with:user.password
-	  
-	  within("#login-content") do
-	  
-     click_button "Login"
-     
-	   
-    end
+	 
+    
+    sign_in user, no_capybara: true
+    
     
     expect do
-            xhr :post, user_groups_path(@user), groups: { name:"Electronics" }
-          end.to change(@user.groups, :count).by(1)
-         # # expect{click_button "Create"}.to change{user.groups.count}.by(1)
-         # expect{click_button "Create Group"}.to change{user.groups.count}.by(1
-     # fill_in "group_name", with: "Electronics"
-#      
-# 
-#      # expect{click_button "Create"}.to change{user.groups.count}.by(1)
-#      expect{click_button "Create Group"}.to change{user.groups.count}.by(1)
+            xhr :post, user_groups_path(user), group: { name:"Electronics" }
+          end.to change(user.groups, :count).by(1)
+       
    end  
 
   it "signed in user should be able see the groups he has created" do
     user = FactoryGirl.create(:user)
+    pending
 	  visit '/'
 	  click_link "Log In"
 
@@ -50,5 +37,27 @@ describe "Groups" do
     expect(page).to have_css "ul#sidebar li"
     # expect(page).to have_css "ul#groupscreated li",text:group2.name
 
+  end
+end
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+	  # visit '/'
+ #     click_link "Log In"
+ # 
+ #     fill_in "session_email",with: user.email
+ #     fill_in "session_password",with:user.password
+ #     
+ #     within("#login-content") do
+ #     
+ #     click_button "Login"
+ #     
+ #      
+ #    end
   end
 end
