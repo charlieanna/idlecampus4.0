71df72d0a8f2b1899e0a76ff7cdaaca72b45ad34
ankur kothari <ankothari@gmail.com>
2013-11-09 19:55:59 +0530

    sidekiq

--- app/workers/pygments_worker.rb
+++ app/workers/pygments_worker.rb
@@ -1,13 +1,10 @@
-require 'net/http'
 class PygmentsWorker
   include Sidekiq::Worker
   
   def perform(args)
-    uri = URI('http://google.com')
-    res = Net::HTTP.get(uri) # => String
-    logger.info  res.body
+    # logger.info  "AAAAASDADASDADAD"
 #     logger.info  args
     
-    # Push.new(args["members"], args["message"]).send_push
+    Push.new(args["members"], args["message"]).send_push
   end
 end
