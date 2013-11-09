978b744822f80ed7262fb1464bb591d8619a5458
ankur kothari <ankothari@gmail.com>
2013-11-09 21:43:22 +0530

    d

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,3 @@
-worker_processes 3
-after_fork do |server, worker|
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
-  end
+before_fork do |server, worker|
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
 end
