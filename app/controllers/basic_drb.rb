24fd0c4f4b73695675094f2f2ea5bb1cd86cac80
ankur kothari <ankothari@gmail.com>
2013-11-18 12:29:01 +0530

    checking if env is development and enaling debugging in that

--- app/controllers/basic_drb.rb
+++ app/controllers/basic_drb.rb
@@ -74,7 +74,7 @@ class TopfunkyIM
   def self.register(username,password)
      jid    = Jabber::JID.new("#{username}@idlecampus.com")
      client = Jabber::Client.new(jid)
-     Jabber::debug = true
+     Jabber::debug = true if Rails.env.development?
      client.connect
      fields = {}
     
