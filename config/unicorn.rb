750dd36f63caa64e796985fbdafe3b1e94eba75d
ankur kothari <ankothari@gmail.com>
2013-11-21 02:14:48 +0530

    sidekiq

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 before_fork do |server, worker|
-   @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+   @sidekiq_pid ||= spawn("bundle exec sidekiq -q carrierwave,5 default")
 end
 
