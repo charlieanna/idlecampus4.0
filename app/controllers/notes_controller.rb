f3b43eda9124ffbdbb34869f3a5d87eeecd3cf60
ankur kothari <ankothari@gmail.com>
2013-11-22 00:33:45 +0530

    for now comented the whole pubsub::service helper class'

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -26,7 +26,7 @@ class NotesController < ApplicationController
     @note.errors.messages
       if @note.save
         xmpp = DRbObject.new_with_uri "druby://localhost:7777"
-        xmpp.publish(text,params["group"])
+        xmpp.publish(@note.message,params["group"])
 
         render :json => @note
       else
