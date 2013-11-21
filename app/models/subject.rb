adde1e156aae81feb4f94917f5f899d66e84ff41
ankur kothari <ankothari@gmail.com>
2013-11-22 01:44:11 +0530

    sadas

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,11 +3,5 @@ class Subject < ActiveRecord::Base
  
 	belongs_to :group
 	belongs_to :timetable
-	def self.in_hash(group)
-		subjects = group.subjects.pluck(:name)
-		 field_entry = {}
-     field_entry["name"] = "subject"
-     field_entry["values"] = subjects.uniq
-     return field_entry
-	end
+	
 end
