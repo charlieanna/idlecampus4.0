d9aa0bd808d6123fbbd9828ab9f045f240d0951d
ankur kothari <ankothari@gmail.com>
2013-11-22 00:00:12 +0530

    reverted timetable entry

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,21 +5,5 @@ class StudentsController < UsersController
     render 'users/new'
   end
   
-  def create
-    @group = Group.find_by(group_code:params[:student][:following_group])
-    
-    if @group
-      super
-      xmpp = DRbObject.new_with_uri "druby://localhost:7777"
-      xmpp.subscribe(@group)
-      @user.type = "Student"
-      
-    
-    else
-     
-      flash[:error] = "Group Not Found"
-      redirect_to new_student_path
-    end
-    
-  end
+ 
 end
--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -2,9 +2,8 @@ class TeachersController < ApplicationController
   def new
     @user = User.new
     @user.rolable_type= "Teacher"
-    
+   render 'users/new'
   end
   
-  def show 
-  end
+  
 end
