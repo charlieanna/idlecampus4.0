6ff16ced5077d5f423f76bca003d3b19863d5e6a
ankur kothari <ankothari@gmail.com>
2013-11-23 11:49:05 +0530

    showing teacher proerly

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
   def show
-    
+    if signed_in?
+      @group = Group.new(user: current_user)
+      @group.group_code = Group.get_group_code
+    end
   end
 end
