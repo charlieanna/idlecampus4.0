815fb13eaff2bce1cbaea2ff98b161a67797d11e
ankur kothari <ankothari@gmail.com>
2013-11-26 02:41:49 +0530

    back to carrierwave

--- app/workers/pygments_worker.rb
+++ app/workers/pygments_worker.rb
@@ -1,7 +1,7 @@
 #
 class PygmentsWorker
   include Sidekiq::Worker
-  sidekiq_options :queue => :push, :retry => false, :backtrace => true
+  sidekiq_options :queue => :carrierwave, :retry => false, :backtrace => true
   def perform(args)
     Push.new(args['members'], args['message'], args['app']).send_push
   end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ timeout 15
 preload_app true
 before_fork do |server, worker|
    @sidekiq_pid ||= spawn("bundle exec sidekiq -q carrierwave,5 default")
-   spawn("bundle exec sidekiq -q push,5 default")
+   # spawn("bundle exec sidekiq -q push,5 default")
 end
 
