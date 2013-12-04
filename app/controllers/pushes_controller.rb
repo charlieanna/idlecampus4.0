f2a0a58813805ca2753ab0817109e82acb626dfc
ankur kothari <ankothari@gmail.com>
2013-12-04 12:40:10 +0530

    removed @ from

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -3,7 +3,7 @@ class PushesController < ApplicationController
   def create
     from = params["from"]
     members = members_in_array(params)
-    args = { from: from,members: members, message: params['message'], app: 'message' }
+    args = { from: from.split("@").first,members: members, message: params['message'], app: 'message' }
     PygmentsWorker.perform_async(args)
     render text: 'OK'
   end
