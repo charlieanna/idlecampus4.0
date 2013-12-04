3b539f8fa5161f42336503f7014282bc6117dcd9
ankur kothari <ankothari@gmail.com>
2013-12-04 12:33:52 +0530

    added a test

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -2,10 +2,16 @@ class PushesController < ApplicationController
   protect_from_forgery except: :create
   def create
     from = params["from"]
-    members = [params['to']]
+    members = members_in_array(params)
     args = { from: from,members: members, message: params['message'], app: 'message' }
     PygmentsWorker.perform_async(args)
     render text: 'OK'
+  end
+  
+  
+  
+  def members_in_array(params)
+    [params["to"]]
   end
 end
 
---
+++ spec/controllers/pushes_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe PushesController do
+
+  describe "GET 'create'",:vcr do
+    it "returns http success" do
+      get 'create'
+      response.should be_success
+    end
+  end
+  
+  describe "#members_in_array" do
+    it "should return the member kept inside an array" do
+      members = PushesController.new.members_in_array({"to"=>"z@idlecampus.com"})
+      expect(members).to eq(["z@idlecampus.com"])
+    end
+  end
+
+end
