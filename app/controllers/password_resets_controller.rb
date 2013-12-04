04e48b3d91c88a8414da94bee98c76b2aa800279
ankur kothari <ankothari@gmail.com>
2013-12-05 05:04:18 +0530

    made the redirect path to the signin_path on reset password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -25,7 +25,7 @@ class PasswordResetsController < ApplicationController
       redirect_to new_password_reset_path, :alert => "Password &crarr; 
         reset has expired."
     elsif @user.update_attributes(params.require(:user).permit(:password, :password_confirmation))
-      redirect_to root_url, :notice => "Password has been reset."
+      redirect_to signin_path, :notice => "Password has been reset."
     else
       render :edit
     end
