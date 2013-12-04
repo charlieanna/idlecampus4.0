7719218d1ad70828de03ff0c07b7f25be1b06bd6
ankur kothari <ankothari@gmail.com>
2013-12-04 12:21:42 +0530

    by mistake removed the aray from members

--- app/controllers/pushes_controller.rb
+++ app/controllers/pushes_controller.rb
@@ -2,7 +2,7 @@ class PushesController < ApplicationController
   protect_from_forgery except: :create
   def create
     from = params["from"]
-    members = params['to']
+    members = [params['to']]
     args = { from: from,members: members, message: params['message'], app: 'message' }
     PygmentsWorker.perform_async(args)
     render text: 'OK'
