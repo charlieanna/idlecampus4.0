8f85642a136c084933566c86938952fcda3a4c00
ankur kothari <ankothari@gmail.com>
2013-12-07 01:29:53 +0530

    revmoed bogus gtip

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,10 +1,6 @@
 
 $(document).ready(function(){
-  $('a').qtip({ // Grab some elements to apply the tooltip to
-      content: {
-          text: 'My common piece of text here'
-      }
-  })
+
  
   
   
